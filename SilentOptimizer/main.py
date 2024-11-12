e5c5780e463f3c07a029d645820da6af37cef8d7
Lutr4nn <145874531+Lutr4nn@users.noreply.github.com>
2024-11-12 18:35:59 +0100

    Refactor project structure: move terminal_menu to SilentOptimizer/menu.py, remove old main.py, and add __init__.py

---
+++ SilentOptimizer/main.py
@@ -0,0 +1,8 @@
+from menu import terminal_menu
+
+def main():
+    terminal_menu()
+    
+if __name__ == "__main__":
+    main()
+    
--- main.py
+++
@@ -1,7 +0,0 @@
-from menu import terminal_menu
-
-def main():
-    terminal_menu()
-
-if __name__ == "__main__":
-    main()
