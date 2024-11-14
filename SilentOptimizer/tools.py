bc9667ce4f0343bc3bd8896c32415be438bfffbf
Lutr4nn <145874531+Lutr4nn@users.noreply.github.com>
2024-11-14 09:59:51 +0100

    Create tools.py
    New files to implement our functions

---
+++ SilentOptimizer/tools.py
@@ -0,0 +1,20 @@
+import psutil
+
+class Tools:
+    
+    def __init__(self):
+        pass
+        
+    def cpu_bouncer(self):
+        """This function kills processes that too much memory."""
+        
+        for process in psutil.process_iter(attrs=['pid', 'name', 'memory_percent']):
+            try:
+                if process.info['memory_percent'] > 2.5: # Valeur à changer mais pour test / trouver quels process il faut kill et quels process il ne faut pas kill
+                    process.kill()
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+
+
+tools = Tools()
+tools.cpu_bouncer()
